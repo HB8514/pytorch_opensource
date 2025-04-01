90bf407020018f1a5b9f213bf0a596c442baa015
HUI <gnlzhd8514@naver.com>
2025-04-01 15:33:26 +0900

    계산기

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -1,18 +1,49 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import (
+    QApplication, QWidget, QPushButton, QVBoxLayout,
+    QHBoxLayout, QMessageBox, QPlainTextEdit
+)
+from PyQt5.QtGui import QIcon
 
-class calculator(QWidget):
-
+class Calculator(QWidget):
     def __init__(self):
         super().__init__()
         self.initUI()
+        
     def initUI(self):
-        self.setWindowTitle('calculator')
-        self.resize(256,256)
+        self.te1 = QPlainTextEdit()
+        self.te1.setReadOnly(True)
+
+        self.btn1 = QPushButton('Message', self)
+        self.btn1.clicked.connect(self.activateMessage)
+
+        self.btn2 = QPushButton('Clear', self)
+        self.btn2.clicked.connect(self.clearMessage)
+
+        hbox = QHBoxLayout()
+        hbox.addStretch(1)
+        hbox.addWidget(self.btn1)
+        hbox.addWidget(self.btn2)
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(self.te1)
+        vbox.addLayout(hbox)
+        vbox.addStretch(1)
+
+        self.setLayout(vbox)
+
+        self.setWindowTitle('2020108084')
+        self.setWindowIcon(QIcon('icon.png'))  # ensure the icon exists in the same directory
+        self.resize(256, 256)
         self.show()
 
-if __name__ == '__main__':
+    def activateMessage(self):
+        self.te1.appendPlainText("Button clicked")
 
+    def clearMessage(self):
+        self.te1.clear()
+
+if __name__ == '__main__':
     app = QApplication(sys.argv)
-    view = calculator()
+    view = Calculator()
     sys.exit(app.exec_())
